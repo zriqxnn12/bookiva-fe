d1b4703257916a495b17e70c930b273175914b03
mhd_azriqin <azriqin@gloriglobalgener.com>
2026-08-14 09:12:55 +0700

    add toaster

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import HomePage from "./pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              borderRadius: "12px",
+              fontFamily: "Sora, sans-serif",
+              fontSize: "14px",
+            },
+          }}
+        />
         <HelmetProvider>
           <Routes>
             <Route path="/" element={<HomePage />} />
